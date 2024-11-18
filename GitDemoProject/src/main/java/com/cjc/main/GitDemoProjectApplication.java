9540a49097d6d52da924714844d0a3eeb2513e3d
hp <hp@LAPTOP-BB603BLN>
2024-11-18 18:44:06 +0530

    my project

---
+++ GitDemoProject/src/main/java/com/cjc/main/GitDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Demo");
+		SpringApplication.run(GitDemoProjectApplication.class, args);
+	}
+
+}
